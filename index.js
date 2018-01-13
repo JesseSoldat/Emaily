af64615820cb14295896e94cd7128d84b4ed50ae
JesseSoldat <jsoldat@hotmail.com>
2018-01-13 15:10:14 +0700

    push for sever

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.send({'jlab': "rocks"});
+  res.send({'jlab': "rocks the house"});
 });
 
 const PORT = process.env.PORT || 5001;
