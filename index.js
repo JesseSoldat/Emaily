4949afb58cfe589573813a58078a0554ac386bcf
JesseSoldat <jsoldat@hotmail.com>
2018-01-13 14:32:11 +0700

    server works

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const app = express();
 
 app.get('*', (req, res) => {
-  res.send({'jlab': rocks});
+  res.send({'jlab': "rocks"});
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT);
