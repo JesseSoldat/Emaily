feba3534a56ec4bd968d732176bc1227ea4dbcb3
JesseSoldat <jsoldat@hotmail.com>
2018-01-16 14:33:46 +0700

    passport and auth routes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+require('./services/passport');
+
 const app = express();
+app.use(bodyParser.json());
+
+require('./routes/authRoutes')(app);
 
 app.get('*', (req, res) => {
   res.send({'jlab': "rocks the house"});
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
