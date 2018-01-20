ea35438c266d55a2fbef561f4824d624b3a62029
JesseSoldat <jsoldat@hotmail.com>
2018-01-20 20:40:05 +0700

    m

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,12 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>, 
 document.getElementById('root'));
-registerServiceWorker();
 
 // console.log('Stripe Key is: ', process.env.REACT_APP_STRIPE_KEY);
 // console.log('ENV', process.env.NODE_ENV);
--- index.js
+++ index.js
@@ -29,9 +29,9 @@ require('./routes/billingRoutes')(app);
 if(process.env.NODE_ENV === 'production') {
   console.log(process.env.NODE_ENV);
 
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
   const path = require('path');
+  
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
